dc1e4cbc1d4e0b693e4c4003ff79f87ca0bb334d
iluckyday <luckyday1990@mail.com>
2020-03-24 12:23:14 +0800

    test

--- download-base-image.sh
+++ download-base-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -ex
+set -e
 
 mkdir -p /tmp/stack && cd /tmp/stack
-version=$(curl -skL https://cloud.debian.org/images/cloud/sid/daily | awk '/href/ {s=$0} END {print s}' | awk -F'"' '{sub(/\//,"",$2);print $2}')
+version=$(curl -skL https://cloud.debian.org/images/cloud/sid/daily | awk '/href/ {s=$0} END {print s}' | awk -F'"' '{sub(/\//,"",$6);print $6}')
 curl -skL https://cloud.debian.org/images/cloud/sid/daily/${version}/debian-sid-nocloud-amd64-daily-${version}.tar.xz | tar -xJ
