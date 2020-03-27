fe337cf9874791e299647030d4387df1eae1b66c
iluckyday <luckyday1990@mail.com>
2020-03-27 20:28:22 +0800

    test

--- create-compute-image.sh
+++ create-compute-image.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 set -e
 
-WORKDIR=/tmp/stack
-MNTDIR=$WORKDIR/mntu
+include_apps="systemd,systemd-sysv,sudo,bash-completion,openssh-server,tzdata,iproute2"
+
+export DEBIAN_FRONTEND=noninteractive
+apt-config dump | grep -we Recommends -e Suggests | sed 's/1/0/' | tee /etc/apt/apt.conf.d/99norecommends
+apt update
+apt install -y debootstrap qemu-system-x86 qemu-utils
+
+MNTDIR=/tmp/debian
 mkdir -p ${MNTDIR}
-cd $WORKDIR
 
-mv disk.raw u.raw
-qemu-img resize -f raw u.raw 203G
-loopx=$(losetup --show -f -P u.raw)
-sgdisk -d 1 $loopx
-sgdisk -N 0 $loopx
-resize2fs -f ${loopx}p1
-tune2fs -O '^has_journal' ${loopx}p1
-mount ${loopx}p1 ${MNTDIR}
-sleep 1
+qemu-img create -f raw /tmp/sid.raw 201G
+loopx=$(losetup --show -f -P /tmp/sid.raw)
+mkfs.ext4 -F -L debian-root -b 1024 -I 128 -O "^has_journal" $loopx
+mount $loopx ${MNTDIR}
 
-cat << EOF >> ${MNTDIR}/etc/fstab
+sed -i 's/ls -A/ls --ignore=lost+found -A/' /usr/sbin/debootstrap
+#/usr/sbin/debootstrap --no-check-gpg --no-check-certificate --cache-dir /tmp --components=main,contrib,non-free --include="$include_apps" --exclude="
+/usr/sbin/debootstrap --no-check-gpg --no-check-certificate --components=main,contrib,non-free --include="$include_apps" sid ${MNTDIR} https://mirrors.aliyun.com/debian
+
+mount -t proc none ${MNTDIR}/proc
+mount -o bind /sys ${MNTDIR}/sys
+mount -o bind /dev ${MNTDIR}/dev
+
+cat << EOF > ${MNTDIR}/etc/fstab
+LABEL=debian-root /          ext4    defaults,noatime              0 0
 tmpfs             /tmp       tmpfs   mode=1777,size=90%            0 0
-tmpfs             /var/log   tmpfs   defautls,noatime              0 0
+tmpfs             /var/log   tmpfs   defaults,noatime              0 0
 EOF
 
 mkdir -p ${MNTDIR}/root/.ssh
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyuzRtZAyeU3VGDKsGk52rd7b/rJ/EnT8Ce2hwWOZWp" >> ${MNTDIR}/root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyuzRtZAyeU3VGDKsGk52rd7b/rJ/EnT8Ce2hwWOZWp" > ${MNTDIR}/root/.ssh/authorized_keys
 chmod 600 ${MNTDIR}/root/.ssh/authorized_keys
 
-mkdir -p ${MNTDIR}/etc/apt/apt.conf.d
-cat << EOF > ${MNTDIR}/etc/apt/apt.conf.d/99-freedisk
+cat << EOF > ${MNTDIR}/etc/apt/apt.conf.d/99freedisk
 APT::Authentication "0";
 APT::Get::AllowUnauthenticated "1";
 Dir::Cache "/dev/shm";
@@ -35,7 +43,11 @@ Dir::Log "/dev/shm";
 DPkg::Post-Invoke {"/bin/rm -f /dev/shm/archives/*.deb || true";};
 EOF
 
-mkdir -p ${MNTDIR}/etc/dpkg/dpkg.cfg.d
+cat << EOF > ${MNTDIR}/etc/apt/apt.conf.d/99norecommend
+APT::Install-Recommends "1";
+APT::Install-Suggests "0";
+EOF
+
 cat << EOF > ${MNTDIR}/etc/dpkg/dpkg.cfg.d/99-nodoc
 path-exclude /usr/share/doc/*
 path-exclude /usr/share/man/*
@@ -44,6 +56,7 @@ path-exclude /usr/share/info/*
 path-exclude /usr/share/lintian/*
 path-exclude /usr/share/linda/*
 path-exclude /usr/share/locale/*
+path-exclude /usr/lib/locale/*
 path-include /usr/share/locale/en*
 EOF
 
@@ -72,12 +85,13 @@ download-cache=/tmp
 cache-dir=/tmp
 EOF
 
+mkdir -p ${MNTDIR}/etc/initramfs-tools/conf.d
 cat << EOF > ${MNTDIR}/etc/initramfs-tools/conf.d/custom
 COMPRESS=xz
 RUNSIZE=50%
 EOF
 
-cat << EOF > ${MNTDIR}/root/.bashrc
+cat << EOF >> ${MNTDIR}/root/.bashrc
 export HISTSIZE=1000 LESSHISTFILE=/dev/null HISTFILE=/dev/null
 EOF
 
@@ -85,10 +99,19 @@ cat << "EOF" > ${MNTDIR}/usr/sbin/stack-install.sh
 #!/bin/bash
 set -ex
 
-APPS="nova-compute neutron-linuxbridge-agent"
-DISABLE_SERVICES="nova-compute neutron-linuxbridge-agent"
+APPS="mariadb-server python3-pymysql \
+rabbitmq-server \
+memcached python3-memcache \
+etcd \
+apache2 libapache2-mod-wsgi \
+python3-openstackclient \
+keystone \
+glance \
+placement-api \
+nova-api nova-conductor nova-novncproxy nova-scheduler \
+neutron-server neutron-linuxbridge-agent neutron-dhcp-agent neutron-metadata-agent neutron-l3-agent"
 
-MASK_SERVICES="e2scrub_all.timer \
+DISABLE_SERVICES="e2scrub_all.timer \
 apt-daily-upgrade.timer \
 apt-daily.timer \
 logrotate.timer \
@@ -99,138 +122,52 @@ e2scrub@.service \
 e2scrub_all.service \
 e2scrub_fail@.service \
 e2scrub_reap.service \
-logrotate.service"
+logrotate.service \
+systemd-timesyncd.service \
+openvswitch-switch.service \
+mysql.service mariadb.service \
+keepalived.service haproxy.service \
+memcached.service \
+rabbitmq-server.service \
+etcd.service \
+apache2.service \
+keystone.service \
+glance-api.service \
+nova-api-metadata.service nova-api.service nova-conductor.service nova-novncproxy.service nova-scheduler.service nova-serialproxy.service nova-spicehtml5proxy.service nova-xenvncproxy.service \
+neutron-api.service neutron-dhcp-agent.service neutron-l3-agent.service neutron-linuxbridge-agent.service neutron-metadata-agent.service neutron-rpc-server.service \
+placement-api.service"
 
-REMOVE_APPS="chrony ifupdown vim unattended-upgrades"
+REMOVE_APPS="ifupdown \
+build-essential \
+gcc-9 \
+libgcc-9-dev \
+g++-9 \
+cpp \
+cpp-9 \
+iso-codes"
 
-DELETE_MODULES="
-fs/udf
-fs/adfs
-fs/affs
-fs/ocfs2
-fs/jfs
-fs/ubifs
-fs/gfs2
-fs/cifs
-fs/befs
-fs/erofs
-fs/hpfs
-fs/f2fs
-fs/xfs
-fs/freevxfs
-fs/hfsplus
-fs/minix
-fs/coda
-fs/dlm
-fs/afs
-fs/omfs
-fs/9p
-fs/reiserfs
-fs/bfs
-fs/qnx6
-fs/nilfs2
-fs/btrfs
-fs/jbd2
-fs/efs
-fs/ceph
-fs/hfs
-fs/jffs2
-fs/orangefs
-fs/ufs
-net/wireless
-net/mpls
-net/wimax
-net/l2tp
-net/nfc
-net/tipc
-net/appletalk
-net/rds
-net/dccp
-net/netrom
-net/lapb
-net/mac80211
-net/6lowpan
-net/sunrpc
-net/rxrpc
-net/atm
-net/psample
-net/rose
-net/ax25
-net/8021q
-net/9p
-net/bluetooth
-net/ife
-net/ceph
-net/phonet
-drivers/media
-drivers/mfd
-drivers/hid
-drivers/nfc
-drivers/dca
-drivers/thunderbolt
-drivers/firmware
-drivers/xen
-drivers/spi
-drivers/i2c
-drivers/uio
-drivers/hv
-drivers/ptp
-drivers/pcmcia
-drivers/isdn
-drivers/atm
-drivers/w1
-drivers/hwmon
-drivers/virt
-drivers/dax
-drivers/parport
-drivers/ssb
-drivers/infiniband
-drivers/gpu
-drivers/bluetooth
-drivers/video
-drivers/android
-drivers/nvme
-drivers/gnss
-drivers/firewire
-drivers/leds
-drivers/net/fddi
-drivers/net/hyperv
-drivers/net/xen-netback
-drivers/net/wireless
-drivers/net/ipvlan
-drivers/net/slip
-drivers/net/usb
-drivers/net/team
-drivers/net/ppp
-drivers/net/bonding
-drivers/net/can
-drivers/net/phy
-drivers/net/vmxnet3
-drivers/net/ieee802154
-drivers/net/fjes
-drivers/net/hippi
-drivers/net/wan
-drivers/net/plip
-drivers/net/appletalk
-drivers/net/wimax
-drivers/net/arcnet
-drivers/net/hamradio
-sound
-"
+mkdir -p /run/systemd/network
+cat << EOFF > /run/systemd/network/20-dhcp.network
+[Match]
+Name=en*
 
+[Network]
+DHCP=ipv4
+EOFF
+
+systemctl start systemd-networkd systemd-resolved
+sleep 2
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y $APPS
 apt remove --purge -y $REMOVE_APPS
 systemctl disable $DISABLE_SERVICES
-systemctl mask $MASK_SERVICES
 
-rm -rf /etc/hostname /etc/resolv.conf /etc/network /usr/share/doc /usr/share/man /tmp/* /var/log/* /var/tmp/* /var/cache/apt/*
-find /usr -type d -name __pycache__ -prune -exec rm -rf {} + || true
-find /usr/*/locale -mindepth 1 -maxdepth 1 ! -name 'en' -prune -exec rm -rf {} + || true
-find /usr/share/zoneinfo -mindepth 1 -maxdepth 2 ! -name 'UTC' -a ! -name 'UCT' -a ! -name 'PRC' -a ! -name 'Asia' -a ! -name '*Shanghai' -prune -exec rm -rf {} + || true
-for m in $DELETE_MODULES; do
-	rm -rf /lib/modules/*/kernel/$m
-done
+systemctl stop mysql etcd
+rm -rf /var/lib/mysql/ib_logfile* /var/lib/etcd/default/member/wal/*
+rm -rf /etc/hostname /etc/resolv.conf /etc/networks /usr/share/doc /usr/share/man /var/tmp/* /var/cache/apt/*
+find /usr -type d -name __pycache__ -prune -exec rm -rf {} +
+find /usr/*/locale -mindepth 1 -maxdepth 1 ! -name 'en' -prune -exec rm -rf {} +
+find /usr/share/zoneinfo -mindepth 1 -maxdepth 2 ! -name 'UTC' -a ! -name 'UCT' -a ! -name 'PRC' -a ! -name 'Asia' -a ! -name '*Shanghai' -prune -exec rm -rf {} +
 EOF
 chmod +x ${MNTDIR}/usr/sbin/stack-install.sh
 
@@ -275,23 +212,50 @@ EOF
 chmod +x ${MNTDIR}/usr/sbin/stack-init.sh
 
 sed -i '/src/d' ${MNTDIR}/etc/apt/sources.list
-sed -i 's/timeout=5/timeout=0/' ${MNTDIR}/boot/grub/grub.cfg
 ( umask 226 && echo 'Defaults env_keep+="PYTHONDONTWRITEBYTECODE PYTHONHISTFILE"' > ${MNTDIR}/etc/sudoers.d/env_keep )
-ln -sf ../usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Shanghai ${MNTDIR}/etc/localtime
 
 for i in stack-install.service stack-init.service
 do
 	ln -sf /etc/systemd/system/$i ${MNTDIR}/etc/systemd/system/multi-user.target.wants/$i
 done
 
-chroot ${MNTDIR} ssh-keygen -A
+mkdir -p ${MNTDIR}/boot/syslinux
+cat << EOF > ${MNTDIR}/boot/syslinux/syslinux.cfg
+PROMPT 0
+TIMEOUT 0
+DEFAULT debian
+
+LABEL debian
+        LINUX /vmlinuz
+        INITRD /initrd.img
+        APPEND root=LABEL=debian-root console=ttyS0 quiet
+EOF
+
+chroot ${MNTDIR} /bin/bash -c "
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin PYTHONDONTWRITEBYTECODE=1 DEBIAN_FRONTEND=noninteractive
+sed -i 's/root:\*:/root::/' etc/shadow
+apt update
+apt install -y -o APT::Install-Recommends=0 -o APT::Install-Suggests=0 linux-image-cloud-amd64 extlinux initramfs-tools
+dd if=/usr/lib/EXTLINUX/mbr.bin of=$loopx
+extlinux -i /boot/syslinux
+
+sed -i '/src/d' /etc/apt/sources.list
+rm -rf /etc/hostname /tmp/apt /usr/share/doc /usr/share/man /tmp/* /var/tmp/* /var/log/* /var/cache/apt/* /var/lib/apt/lists/*
+find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en' -exec rm -rf {} +
+find /usr/share/zoneinfo -mindepth 1 -maxdepth 2 ! -name 'UTC' -a ! -name 'UCT' -a ! -name 'PRC' -a ! -name 'Asia' -a ! -name '*Shanghai' -exec rm -rf {} +
+"
+
 sync ${MNTDIR}
 sleep 1
+umount ${MNTDIR}/dev ${MNTDIR}/proc ${MNTDIR}/sys
 umount ${MNTDIR}
 sleep 1
 losetup -d $loopx
 
-qemu-system-x86_64 -name stack-u-building -machine q35,accel=kvm -cpu host -smp "$(nproc)" -m 6G -nographic -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -boot c -drive file=$WORKDIR/u.raw,if=virtio,format=raw,media=disk -netdev user,id=n0,ipv6=off -device virtio-net,netdev=n0
-qemu-img convert -f raw -O qcow2 $WORKDIR/u.raw /dev/shm/stack-u.img
+qemu-system-x86_64 -name stack-c-building -machine q35,accel=kvm -cpu host -smp "$(nproc)" -m 4G -nographic -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -boot c -drive file=/tmp/sid.raw,if=virtio,format=raw,media=disk -netdev user,id=n0,ipv6=off -device virtio-net,netdev=n0
 
-ls -lh /dev/shm/stack-u.img
+qemu-img convert -c -f raw -O qcow2 /tmp/sid.raw /dev/shm/stack-c.img
+ls -lh /dev/shm/stack-c.img
+
+exit 0
