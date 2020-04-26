7fa8a5727a908ed761d15169dec4e4b00dfd0f59
iluckyday <luckyday1990@mail.com>
2020-04-26 12:55:47 +0800

    test

--- create-compute-image.sh
+++ create-compute-image.sh
@@ -101,7 +101,7 @@ systemd-timesyncd.service \
 libvirtd.service libvirt-guests.service \
 nova-compute neutron-linuxbridge-agent"
 
-REMOVE_APPS=""
+REMOVE_APPS="tzdata"
 
 mkdir -p /run/systemd/network
 cat << EOFF > /run/systemd/network/20-dhcp.network
@@ -116,7 +116,7 @@ systemctl start systemd-networkd systemd-resolved
 sleep 2
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y $APPS
-#dpkg -P --force-depends $REMOVE_APPS
+dpkg -P --force-depends $REMOVE_APPS
 systemctl disable $DISABLE_SERVICES
 
 rm -rf /etc/hostname /etc/resolv.conf /etc/networks /usr/share/doc /usr/share/man /var/tmp/* /var/cache/apt/*
