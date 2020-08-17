9df718b0107903abf1d7dedb70b90d4892499ce5
iluckyday <luckyday1990@mail.com>
2020-08-17 12:02:44 +0800

    test

---
+++ acr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+region=cn-chengdu
+namespace=iimage
+tag=$(date '+%Y%m%d')
+
+for f in /dev/shm/stack-*.img; do
+        filename="$(basename $f)"
+        filepath="$(dirname $f)"
+
+        image="$filename:$tag"
+        cd $filepath
+        tar -c $filename | docker import - registry.${region}.aliyuncs.com/${namespace}/${image}
+        echo "$ACR_DOCKER_PASSWORD" | docker login -u "$ACR_DOCKER_USERNAME" --password-stdin registry.${region}.aliyuncs.com
+        docker push registry.${region}.aliyuncs.com/${namespace}/${image}
+        docker logout registry.${region}.aliyuncs.com
+done
