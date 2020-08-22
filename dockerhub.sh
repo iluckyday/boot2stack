c30738c25a9258049c9c350313b63553f69d20eb
moyuexue <moyuexue@163.com>
2020-08-22 21:30:46 +0800

    test

--- dockerhub.sh
+++ dockerhub.sh
@@ -11,7 +11,9 @@ for f in /dev/shm/stack-*.img; do
         tag="${filename}_$(date '+%Y%m%d')"
         cd $filepath
         tar -c $filename | docker import - ${user}/${image}:${tag}
+        docker tag ${user}/${image}:${tag} ${user}/${image}:latest
         echo "$DOCKERHUB_PASS" | docker login -u "$user" --password-stdin
         docker push ${user}/${image}:${tag}
+        docker push ${user}/${image}:latest
         docker logout
 done
