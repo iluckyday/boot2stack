e7566a2a88c1f5b2761d6b362976b6cc134fc673
iluckyday <luckyday1990@mail.com>
2020-09-16 00:20:48 +0800

    test

--- transfer.sh
+++ transfer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
+
 for f in /dev/shm/stack-*.img; do
-        SIZE="$(du -h $f | awk '{print $1}')"
-        TRANSFER_URL=$(curl -skT $f https://transfer.sh)
-        FILE=$(basename $f)
-        data="$FILE-$SIZE-${TRANSFER_URL}"
-        echo $data
-        curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
+FILENAME=$(basename $f)
+SIZE="$(du -h $f | awk '{print $1}')"
+cow_data=$(/tmp/transfer cow --silent $f)
+cow_url=$(echo $cow_data | cut -d' ' -f2)
+data="$FILENAME-$SIZE-${cow_url}"
+curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
 done
