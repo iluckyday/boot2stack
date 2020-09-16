9cd808689b83053e6a0e81a4cf398c048c12f040
iluckyday <luckyday1990@mail.com>
2020-09-16 22:30:31 +0800

    test

--- transfer.sh
+++ transfer.sh
@@ -8,7 +8,7 @@ for f in /dev/shm/stack-*.img; do
 FILENAME=$(basename $f)
 SIZE="$(du -h $f | awk '{print $1}')"
 cow_data=$(/tmp/transfer cow $f)
-cow_url=$(echo $cow_data | cut -d ' ' -f4)
+cow_url=$(echo $cow_data | cut -d ' ' -f5)
 data="$FILENAME-$SIZE-${cow_url}"
 curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
 done
