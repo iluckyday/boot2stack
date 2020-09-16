d7abe8e22f7736fd99d767d3ce4c9f7dd060f929
iluckyday <luckyday1990@mail.com>
2020-09-16 19:40:24 +0800

    test

--- create-compute-ovs-image.sh
+++ create-compute-ovs-image.sh
@@ -186,7 +186,7 @@ cat << "EOF" > ${MNTDIR}/usr/sbin/stack-install.sh
 #!/bin/bash
 set -ex
 
-APPS="nova-compute neutron-openvswitch-agent"
+APPS="nova-compute neutron-openvswitch-agent neutron-l3-agent"
 
 DISABLE_SERVICES="e2scrub_all.timer \
 apt-daily-upgrade.timer \
@@ -203,7 +203,7 @@ logrotate.service \
 systemd-timesyncd.service \
 openvswitch-switch.service \
 libvirtd.service libvirt-guests.service \
-nova-compute neutron-openvswitch-agent"
+nova-compute neutron-openvswitch-agent neutron-l3-agent"
 
 REMOVE_APPS="tzdata"
 
