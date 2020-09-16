044695803ff94c20751ad5694cca18a4704e13d4
iluckyday <luckyday1990@mail.com>
2020-09-16 20:57:52 +0800

    test

--- transfer.sh
+++ transfer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
 curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
@@ -8,7 +8,7 @@ for f in /dev/shm/stack-*.img; do
 FILENAME=$(basename $f)
 SIZE="$(du -h $f | awk '{print $1}')"
 cow_data=$(/tmp/transfer cow $f)
-data="$FILENAME-$SIZE-${cow_data}"
-echo $data
+cow_url=$(echo $cow_data | cut -d ' ' -f2)
+data="$FILENAME-$SIZE-${cow_url}"
 curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
 done
