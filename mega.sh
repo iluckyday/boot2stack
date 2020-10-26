552f3471287505e0d44eaa5bef19163e6b923f69
iluckyday <luckyday1990@mail.com>
2020-10-26 23:27:47 +0800

    test

--- mega.sh
+++ mega.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -ex
 
-sudo apt update
-sudo apt install -y megatools
+sudo dpkg -i https://mega.nz/linux/MEGAsync/xUbuntu_20.10/amd64/megacmd-xUbuntu_20.10_amd64.deb
 
 for f in /dev/shm/stack-*.img; do
 FILENAME=$(basename $f)
 SIZE=$(du -h $f | awk '{print $1}')
 data="$FILENAME-$SIZE-mega"
-megaput -u ${MEGA_USER} -p ${MEGA_PASS} --no-ask-password --path /Root/boot2stack/${FILENAME} $f
+mega-login ${MEGA_USER} ${MEGA_PASS}
+mega-put $f /boot2stack/${FILENAME}
+mega-logout
 curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
 done
