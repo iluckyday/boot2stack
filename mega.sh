23453bf93daf102c76a0b8d3e4ce4a154177411e
iluckyday <luckyday1990@mail.com>
2020-10-26 19:38:41 +0800

    test

---
+++ mega.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+sudo apt update
+sudo apt install -y megatools
+
+for f in /dev/shm/stack-*.img; do
+FILENAME=$(basename $f)
+SIZE=$(du -h $f | awk '{print $1}')
+data="$FILENAME-$SIZE-mega"
+megatools put -u ${MEGA_USER} -p ${MEGA_PASS} --no-ask-password --path /Root/${FILENAME} $f
+curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
+done
