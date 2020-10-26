fd6e10908f4b1be6a4ab0066c7e210e1e654b401
iluckyday <luckyday1990@mail.com>
2020-10-27 00:55:56 +0800

    test

--- mega.sh
+++ mega.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 
-sudo dpkg -i https://mega.nz/linux/MEGAsync/xUbuntu_20.10/amd64/megacmd-xUbuntu_20.10_amd64.deb
+cd /tmp
+curl -skLO https://mega.nz/linux/MEGAsync/xUbuntu_20.10/amd64/megacmd-xUbuntu_20.10_amd64.deb
+sudo dpkg -i /tmp/megacmd-xUbuntu_20.10_amd64.deb
 mega-login ${MEGA_USER} ${MEGA_PASS}
 mega-logout
 
