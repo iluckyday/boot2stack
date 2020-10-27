62c84f63d5c54eaa5d05c9fac8b8df9261ddb220
iluckyday <luckyday1990@mail.com>
2020-10-27 15:40:54 +0800

    test

--- mega.sh
+++ mega.sh
@@ -18,7 +18,7 @@ for f in /dev/shm/stack-*.img; do
 FILENAME=$(basename $f)
 SIZE=$(du -h $f | awk '{print $1}')
 data="$FILENAME-$SIZE-mega"
-mega-put $f /boot2stack/${FILENAME}
+mega-put $f /boot2stack
 curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
 done
 
